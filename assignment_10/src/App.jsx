4eb53263d7ed4b2a11ab8eadd8df6c72a77acc25
satyanishika-31 <satya.nishika@gmail.com>
2026-03-27 14:05:51 +0530

    Add assignment_10 project

---
+++ assignment_10/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import NavBar from '../Components/NavBar'
+import Body from '../Components/Body'
+
+function App() {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <NavBar/>
+      <Body />
+    </div>
+  )
+}
+
+export default App
